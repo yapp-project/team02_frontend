d239d9afa9902188fb52c541873bf6b8a7fe58e5
AnGwangHo <akh4444@nate.com>
2019-03-11 00:06:37 +0900

    - router 설정 - 불필요한 파일 삭제

---
+++ src/views/MainView/MainView.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import * as styles from "./MainView.scss";
+
+class MainView extends Component {
+  render() {
+    return (
+      <div>
+        <div className={styles.body}>왜적용이안되죠?</div>
+      </div>
+    );
+  }
+}
+
+export default MainView;
