d903b5c4e22dd93003c7d4dffc4b0452b66c9c38
AnGwangHo <akh4444@nate.com>
2019-03-25 01:00:22 +0900

    ### #7 검색 결과 페이지 레이아웃 See: Github issue #7 --------------------------------------- ### 작업파일 * components/Combo/Combo.jsx * components/Popup/Poppup.scss * components/SearchResultItem/SearchResultItem.jsx/scss * containers/MainView/MainView.jsx/scss * containers/SearchPopup/SearchPopup.jsx/scss * containers/SearchResult/SearchResult.jsx/scss --------------------------------------- ### 작업내용 * images폴더 추가, 좋아요 icon/칵테일 썸네일사진 임시 추가 * SearchResultItem Component추가, Combo hight수정을 위해 props 추가, Popup z-index 추가 * MainView 정리 및 v-scroll 불가능 하도록 수정 * SearchPopup에 검색 누를 시 결과 나오도록 임시로 추가(버튼 클릭 할 시 나오고 다시 클릭 시 사라짐) -> saga작업 필요 * SearchResult라는 칵테일 검색 결과를 보여주는 container 생성(인자로 data만 넘김)

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,6 @@
 import MainView from "./MainView/MainView";
 import LoginPopup from "./LoginPopup/LoginPopup";
 import SearchPopup from "./SearchPopup/SearchPopup";
+import SearchResult from "./SearchResult/SearchResult";
 
-export { MainView, LoginPopup, SearchPopup };
+export { MainView, LoginPopup, SearchPopup, SearchResult };
