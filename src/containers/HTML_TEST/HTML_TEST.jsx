b92ed5e45acae5355e0e50d0c369f1f26b22daa7
SungJin <osj4872@gmail.com>
2019-03-29 17:37:10 +0900

    #16 레시피 보기 See: Github issue #16 --------------------------------------- ### 작업파일 * HTML_TEST 폴더 내의 파일 --------------------------------------- ### 작업내용 * 테스트 페이지로 레이아웃 영역 잡는중 * 깃허브에 이슈가 생성이 안되서 현재 16번 이슈로 예상하고 생성

--- src/containers/HTML_TEST/HTML_TEST.jsx
+++ src/containers/HTML_TEST/HTML_TEST.jsx
@@ -34,7 +34,48 @@ class Header extends Component {
     const { bShowSearch } = this.state;
 
     return (
-      <Div content={this.common_form("title", "detail")}></Div>
+      // <Div content={this.common_form("title", "detail")}></Div>
+      <div className={cx("detail-container")}>
+        <div className={cx("detail-content")}>
+          <div className={cx("detail-content-header")}>
+            <span>칵테일 이름</span>
+
+            <div className={cx("detail-content-header-info")}>
+              <span>등록한 사람 닉네임</span>
+
+              <div className={cx("detail-content-header-info-count")}>
+                <span style={{display: "inline-block", width: "16px", height: "16px", border: "1px solid #707070"}}></span>
+                <span>85</span>
+                <span style={{display: "inline-block", width: "16px", height: "16px", border: "1px solid #707070"}}></span>
+                <span>24</span>
+              </div>
+            </div>
+          </div>
+
+          <div className={cx("detail-content-main")}>
+            <div className={cx("detail-content-main-view")}>
+              <span className={cx("detail-content-main-view-cup show")}></span>
+
+              <span className={cx("detail-content-main-view-image close")}></span>
+            </div>
+
+            <div className={cx("detail-content-main-side")}>
+              <div className={cx("detail-content-main-side-toolbar")}>
+                <span><text>정보</text></span>
+                <span><text>재료</text></span>
+                <span><text>사진</text></span>
+                <span>댓글</span>
+              </div>
+
+              <div className={cx("detail-content-main-side-info")}>
+
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
     );
   }
 }
