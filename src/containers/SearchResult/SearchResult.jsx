df145b1f39bb37c8b99cfcec6d3f6f3a540b8d2e
AnGwangHo <akh4444@nate.com>
2019-05-02 23:23:54 +0900

    ### #14 검색 결과 레이아웃 반응형 기능 See: Github issue #14 --------------------------------------- ### 작업파일 * SearchResult.jsx * SearchResult.scss --------------------------------------- ### 작업내용 * 검색 결과 반응형 결과로 나오도록 반영(#24 pull한 다음 결과 반영)

--- src/containers/SearchResult/SearchResult.jsx
+++ src/containers/SearchResult/SearchResult.jsx
@@ -4,8 +4,7 @@ import styles from "./SearchResult.scss";
 import { connect } from "react-redux";
 import { SearchResultItem } from "../../components";
 
-import cocktail1 from "../../static/images/a1.jpeg";
-import cocktail2 from "../../static/images/a2.jpg";
+import { GridLayout } from "@egjs/react-infinitegrid";
 
 const cx = classNames.bind(styles);
 
@@ -14,51 +13,62 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = {};
-const dummy_data = [
-  { no: 0, Image: cocktail1, name: "칵테일1", like: 10 },
-  { no: 1, Image: cocktail2, name: "칵테일2", like: 20 },
-  { no: 2, Image: cocktail1, name: "칵테일3", like: 30 },
-  { no: 3, Image: cocktail2, name: "칵테일4", like: 40 },
-  { no: 4, Image: cocktail1, name: "칵테일5", like: 50 },
-  { no: 5, Image: cocktail2, name: "칵테일6", like: 60 },
-  { no: 6, Image: cocktail1, name: "칵테일7", like: 70 },
-  { no: 7, Image: cocktail2, name: "칵테일8", like: 80 },
-  { no: 8, Image: cocktail1, name: "칵테일9", like: 90 },
-  { no: 9, Image: cocktail2, name: "칵테일10", like: 100 }
-];
+
+const Item = ({ num }) => (
+  <SearchResultItem
+    className={cx("item")}
+    Image={`https://naver.github.io/egjs-infinitegrid/assets/image/${(num %
+      59) +
+      1}.jpg`}
+    key={num}
+    name={num}
+    like={num}
+  />
+);
 
 /**
  * @author AnGwangHo
  * @description 검색 결과를 표현하는 컨테이너
  */
 class SearchResult extends Component {
-  /**
-   * @author AnGwangHo
-   * @description 칵테일 검색결과 레이아웃
-   * @param data 검색결과 data[{no, Image, name},{}...]
-   */
-  result_form = ({ data }) => {
-    return data.map(item => {
-      return (
-        <ui className={cx("resultitem")} key={item.name}>
-          <SearchResultItem
-            className={cx("image_container")}
-            key={item.no}
-            props={item}
-            like={item.like}
-          />
-        </ui>
+  state = { list: [] };
+  loadItems(groupKey, num) {
+    const items = [];
+    const start = this.start || 0;
+
+    for (let i = 0; i < num; ++i) {
+      items.push(
+        <Item groupKey={groupKey} num={1 + start + i} key={start + i} />
       );
-    });
+    }
+    this.start = start + num;
+    return items;
+  }
+  onAppend = ({ groupKey, startLoading }) => {
+    startLoading();
+    const list = this.state.list;
+    const items = this.loadItems(parseFloat(groupKey) + 1, 5);
+
+    this.setState({ list: list.concat(items) });
+  };
+  onLayoutComplete = ({ isLayout, endLoading }) => {
+    !isLayout && endLoading();
   };
 
   render() {
     const { data } = this.props;
     return (
-      <div>
-        <ul className={cx("searchresultform")}>
-          {this.result_form({ data: data })}
-        </ul>
+      <div className={cx("result_form")}>
+        <GridLayout
+          margin={10}
+          align="center"
+          onAppend={this.onAppend}
+          onLayoutComplete={this.onLayoutComplete}
+          transitionDuration={0.2}
+          isConstantSize={true}
+        >
+          {this.state.list}
+        </GridLayout>
       </div>
     );
   }
